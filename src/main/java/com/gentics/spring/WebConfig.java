16fc9a7895b1290df416026050121360aa1fe410
Leonard Osang <l.osang@gentics.com>
2014-04-10 21:58:24 +0200

    Initial step.

---
+++ src/main/java/com/gentics/spring/WebConfig.java
@@ -0,0 +1,30 @@
+package com.gentics.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+	
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[0];
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { WebAppConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	@Configuration
+	@EnableWebMvc
+	@ComponentScan("com.gentics.spring.controller")
+	public static class WebAppConfig {
+	}
+}
