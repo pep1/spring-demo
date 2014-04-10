6470721cd5d4524e0004a0fff205457092248fe6
Leonard Osang <l.osang@gentics.com>
2014-04-10 22:17:03 +0200

    Added step 2: basic REST demo.

---
+++ src/main/java/com/gentics/spring/controller/RestExample.java
@@ -0,0 +1,23 @@
+package com.gentics.spring.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.gentics.spring.model.Greeting;
+
+@Controller
+public class RestExample {
+	
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping("/greeting")
+	public @ResponseBody
+	Greeting greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+}
